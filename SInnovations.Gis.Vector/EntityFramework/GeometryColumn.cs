dea5eda53b6bead3b4f698f468787ef44e190119
Poul Kjeldager Sørensen <pks@s-innovations.net>
2014-10-14 23:49:17 +0200

    Moved VectorTiles into Vector project instead. (naming)

---
+++ SInnovations.Gis.Vector/EntityFramework/GeometryColumn.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SInnovations.Gis.VectorTiles.EntityFramework
+{
+    [Table("geometry_columns")]
+    public class GeometryColumn
+    {
+        [Key]
+        [Column("f_table_catalog", Order = 1)]
+
+        public string TableCatalog { get; set; }
+        [Key]
+        [Column("f_table_schema", Order = 2)]
+        public string TableSchema { get; set; }
+        [Key]
+        [Column("f_table_name", Order = 3)]
+        public string TableName { get; set; }
+        [Key]
+        [Column("f_geometry_column", Order = 4)]
+        public string GeometryColumnName { get; set; }
+    }
+}
